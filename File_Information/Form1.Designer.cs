f49f858f480b44e320280efe6ee496d23ec28928
ladyvamp <ladyvamp666@gmail.com>
2018-01-29 21:18:41 +0300

    Init

---
+++ File_Information/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace File_Information
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.rbDoc = new System.Windows.Forms.RadioButton();
+            this.rbRtf = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(60, 245);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(60, 273);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 17);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(60, 299);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 17);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "label8";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(159, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 29);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Выбрать файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(63, 363);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(401, 96);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // rbDoc
+            // 
+            this.rbDoc.AutoSize = true;
+            this.rbDoc.Location = new System.Drawing.Point(63, 32);
+            this.rbDoc.Name = "rbDoc";
+            this.rbDoc.Size = new System.Drawing.Size(52, 21);
+            this.rbDoc.TabIndex = 10;
+            this.rbDoc.TabStop = true;
+            this.rbDoc.Text = "doc";
+            this.rbDoc.UseVisualStyleBackColor = true;
+            // 
+            // rbRtf
+            // 
+            this.rbRtf.AutoSize = true;
+            this.rbRtf.Location = new System.Drawing.Point(63, 59);
+            this.rbRtf.Name = "rbRtf";
+            this.rbRtf.Size = new System.Drawing.Size(42, 21);
+            this.rbRtf.TabIndex = 11;
+            this.rbRtf.TabStop = true;
+            this.rbRtf.Text = "rtf";
+            this.rbRtf.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 471);
+            this.Controls.Add(this.rbRtf);
+            this.Controls.Add(this.rbDoc);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RadioButton rbDoc;
+        private System.Windows.Forms.RadioButton rbRtf;
+    }
+}
+
---
+++ File_Information/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Information
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            rbDoc.Checked = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((rbDoc.Checked == true) && (rbRtf.Checked == false))
+                LoadMyDoc();
+            else if ((rbRtf.Checked == true) && (rbDoc.Checked == false))
+                LoadMyRtf();
+        }
+
+
+        public void LoadMyDoc()
+        {
+            // Create an OpenFileDialog to request a file to open.
+            OpenFileDialog f = new OpenFileDialog();
+
+            // Initialize the OpenFileDialog to look for RTF files.
+            f.DefaultExt = "*.doc";
+            f.Filter = "DOC Files|*.doc";
+
+            // Determine whether the user selected a file from the OpenFileDialog.
+            if (f.ShowDialog() == System.Windows.Forms.DialogResult.OK &&
+               f.FileName.Length > 0)
+            {
+                // Load the contents of the file into the RichTextBox.
+                richTextBox1.LoadFile(f.FileName);
+            }
+
+            //if (f.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            //{
+                GetFileInformation(f.FileName);
+            //}
+        }
+
+        public void LoadMyRtf()
+        {
+            OpenFileDialog f = new OpenFileDialog();
+
+            f.DefaultExt = "*.rtf";
+            f.Filter = "RTF Files|*.rtf";
+
+            if (f.ShowDialog() == System.Windows.Forms.DialogResult.OK &&
+               f.FileName.Length > 0)
+            {
+                richTextBox1.LoadFile(f.FileName);
+            }
+
+            //if (f.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            //{
+                GetFileInformation(f.FileName);
+            //}
+        }
+
+        private void GetFileInformation(string fileName)
+        {
+            FileInfo f = new FileInfo(fileName);
+
+            label1.Text = "Name - " + f.Name.ToString();
+            label2.Text = "File Extension - " + f.Extension;
+            label3.Text = "Creation Time - " + f.CreationTime.ToString();
+            label4.Text = "Last Access Time - " + f.LastAccessTime.ToString();
+            label5.Text = "Last Write Time - " + f.LastWriteTime.ToString();
+            label6.Text = "Directory Name - " + f.DirectoryName;
+            label7.Text = "Full Name - " + f.FullName;
+            label8.Text = "File Size - " + (f.Length / 1024).ToString() + "KB";
+        }
+    }
+}
